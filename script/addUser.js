ed34bda31e6397adb834c091bcd95b1d996e466f
nanyuanbeizhe <36node@gmail.com>
2012-10-10 14:34:14 +0800

    change script

---
+++ script/addUser.js
@@ -0,0 +1,18 @@
+var Food = require('../models/food.js')
+  , User = require('../models/user.js')
+  , cryptUtil = require('../utils/crypt.js')
+  , mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/pantry-test');
+
+var user = new User;
+user.email = 'xinyuan@36node.com';
+user.password = cryptUtil.md5('xinyuan');
+user.name = '鑫缘';
+user.active = true;
+user.roles.push({name: 'shopAdmin'});
+user.save(function(err){
+  if(err){
+    return console.log('User save failed: ' + user.name);
+  }
+});
