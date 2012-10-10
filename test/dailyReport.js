9ef8e101b4eaf2f900d9cb5cfdc0cebfacb418c8
unknown <juwang@CHN-JUWANG.corp.microstrategy.com>
2012-10-10 13:38:48 +0800

    mail dailyreport at 2012-10-09

--- test/dailyReport.js
+++ test/dailyReport.js
@@ -4,15 +4,15 @@ var Order = require('../models/order.js')
 
 mongoose.connect('mongodb://localhost/pantry');
     
-    var start = new Date();
-    var end = new Date();
-    start.setHours(0);
-    start.setMinutes(0);
-    start.setSeconds(0);
-    end.setHours(23);
-    end.setSeconds(59);
-    end.setMinutes(59);
+var start = new Date('2012-10-09');
+var end = new Date('2012-10-09');
+start.setHours(0);
+start.setMinutes(0);
+start.setSeconds(0);
+end.setHours(23);
+end.setSeconds(59);
+end.setMinutes(59);
 
-    Order.find({'date': {'$gte': start, '$lte': end}}, function(err,orders){
-      mailUtil.sendDailyOrders(orders);
-    });
+Order.find({'date': {'$gte': start, '$lte': end}}, function(err,orders){
+  mailUtil.sendDailyOrders(orders);
+});
