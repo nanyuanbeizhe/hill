4f6eb5394c1108a04212d80a17cb176252898477
zzswang <zzswang@gmail.com>
2012-10-11 23:00:29 +0800

    v0.2.0 ready

--- public/js/frame.js
+++ public/js/frame.js
@@ -12,4 +12,23 @@ $(function () {
 			return this;		
 		}
 	});
+
+	App.View.Notice = Backbone.View.extend({
+		templateId : 'tpl-notice-head',
+		events: {
+			"click button":   "closeNotice"
+		},
+
+		closeNotice: function() {
+			$.cookie('never_notice', $('#never-show').val());
+		},
+
+		render: function() {
+			if($.cookie('never_notice')) return this;
+			
+			this.template = _.template(loadTemplate(this.templateId));
+			$(this.el).html(this.template());
+			return this;
+		}
+	});
 });
