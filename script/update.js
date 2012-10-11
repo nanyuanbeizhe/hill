692d1d6031c0a372344e51fd1dffcb5a4737bc30
zzswang <zzswang@gmail.com>
2012-10-11 08:41:22 +0800

    update db

--- script/update.js
+++ script/update.js
@@ -5,13 +5,13 @@ var Food = require('../models/food.js')
 
 mongoose.connect('mongodb://localhost/pantry');
 
-User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
-	if(err) return console.log('can not find user: xinyuan');
+//User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
+//	if(err) return console.log('can not find user: xinyuan');
 
 	Food.findById('504c934b8ba15c710d000003', function(err, food){
 		if(err) return console.log(err);
-		food.description = '米饭 酱香大鸡腿 扁豆焖肉 肉末豆腐';
-		food.shopAdmin = user._id;
+		food.description = '米饭 土豆炖牛肉 水煮肉 耗油生菜';
+		//food.shopAdmin = user._id;
 		food.save(function (err, food){
 			if(err) return console.log(err);
 			console.log('8 set food updated: ');
@@ -21,8 +21,8 @@ User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
 
 	Food.findById('504c934b8ba15c710d000005', function(err, food){
 		if(err) return console.log(err);
-		food.description = '米饭 酱香大鸡腿 扁豆肉片 肉末豆腐 豆豉鳞鱼 油麦菜 水果';
-		food.shopAdmin = user._id;
+		food.description = '米饭 土豆炖牛肉 水煮肉 耗油生菜 芹菜花生米 水果';
+		//food.shopAdmin = user._id;
 		food.save(function (err, food){
 			if(err) return console.log(err);
 			console.log('10 set food updated: ');
@@ -32,8 +32,8 @@ User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
 
 	Food.findById('504c934b8ba15c710d000007', function(err, food){
 		if(err) return console.log(err);
-		food.description = '米饭 酱香大鸡腿 糖醋里脊 扁豆焖肉 肉末豆腐 豆豉鳞鱼油 麦菜 饮料或酸奶';
-		food.shopAdmin = user._id;
+		food.description = '米饭 土豆炖牛肉 水煮肉片 香辣肉丝 蚝油生菜 芹菜花生米 饮料或酸奶';
+		//food.shopAdmin = user._id;
 		food.save(function (err, food){
 			if(err) return console.log(err);
 			console.log('12 set food updated: ');
@@ -41,7 +41,7 @@ User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
 		});
 	});
 
-	Food.findById('504c934b8ba15c710d000009', function(err, food){
+/*	Food.findById('504c934b8ba15c710d000009', function(err, food){
 		if(err) return console.log(err);
 		food.shopAdmin = user._id;
 		food.save(function (err, food){
@@ -49,6 +49,6 @@ User.findOne({'email':'xinyuan@36node.com'}, function(err, user){
 			console.log('rice updated: ');
 			console.log(food);
 		});
-	});
+	});*/
 
-});
+//});
